ae30a132388d1b749d5356ccef3a5e2dc0741f62
Tomas Jelinek <jelkosz@gmail.com>
2012-04-11 21:19:53 +0200

    Recursive generation of the board

---
+++ kanbanik-server/src/test/scala/com/googlecode/kanbanik/model/BuildTestableWorkflow.scala
@@ -0,0 +1,94 @@
+package com.googlecode.kanbanik.model
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.Spec
+import com.mongodb.casbah.commons.MongoDBObject
+import org.bson.types.ObjectId
+
+// This actually tests nothing, only prepares the DB to
+// contain some data
+@RunWith(classOf[JUnitRunner])
+class BuildTestableWorkflow extends Spec {
+
+  describe("This actually tests nothing, only prepares the DB to contain some data") {
+
+    it("should create a complex data structure with all the features") {
+      DataLoader.clearDB
+      // boards
+      DataLoader.boards += MongoDBObject(
+        "_id" -> new ObjectId("2f48e10644ae3742baa2d0b9"),
+        "name" -> "board1 name",
+        "workflowitems" -> List(
+          new ObjectId("3f48e10644ae3742baa2d0b9"),
+          new ObjectId("4f48e10644ae3742baa2d0b9")))
+
+      DataLoader.projects += MongoDBObject(
+        "_id" -> new ObjectId("2a48e10644ae3742baa2d0d9"),
+        "name" -> "project1",
+        "boards" -> List(
+          new ObjectId("2f48e10644ae3742baa2d0b9")),
+        "tasks" -> None)
+
+      DataLoader.projects += MongoDBObject(
+        "_id" -> new ObjectId("2a48e10644ae3742baa2d0d9"),
+        "name" -> "project2",
+        "boards" -> List(
+          new ObjectId("2f48e10644ae3742baa2d0b9")),
+        "tasks" -> None)
+
+      // workflowitems    
+      DataLoader.workflowitems += MongoDBObject(
+        "_id" -> new ObjectId("3f48e10644ae3742baa2d0b9"),
+        "name" -> "name1",
+        "wipLimit" -> 4,
+        "childId" -> None,
+        "nextItemId" -> Some(new ObjectId("4f48e10644ae3742baa2d0b9")),
+        "boardId" -> new ObjectId("1e48e10644ae3742baa2d0b9"))
+      DataLoader.workflowitems += MongoDBObject(
+        "_id" -> new ObjectId("4f48e10644ae3742baa2d0b9"),
+        "name" -> "name2",
+        "wipLimit" -> 4,
+        "childId" -> new ObjectId("5f48e10644ae3742baa2d0b9"),
+        "nextItemId" -> None,
+        "boardId" -> new ObjectId("1e48e10644ae3742baa2d0b9"))
+
+      DataLoader.workflowitems += MongoDBObject(
+        "_id" -> new ObjectId("5f48e10644ae3742baa2d0b9"),
+        "name" -> "name2-1",
+        "wipLimit" -> 4,
+        "childId" -> None,
+        "nextItemId" -> new ObjectId("6f48e10644ae3742baa2d0b9"),
+        "boardId" -> new ObjectId("6f48e10644ae3742baa2d0b9"))
+      DataLoader.workflowitems += MongoDBObject(
+        "_id" -> new ObjectId("6f48e10644ae3742baa2d0b9"),
+        "name" -> "name2-2",
+        "wipLimit" -> 4,
+        "childId" -> new ObjectId("6f48e10644ae3742baa2d0b0"),
+        "nextItemId" -> new ObjectId("7f48e10644ae3742baa2d0b9"),
+        "boardId" -> new ObjectId("1e48e10644ae3742baa2d0b9"))
+      DataLoader.workflowitems += MongoDBObject(
+        "_id" -> new ObjectId("7f48e10644ae3742baa2d0b9"),
+        "name" -> "name2-3",
+        "wipLimit" -> 4,
+        "childId" -> None,
+        "nextItemId" -> None,
+        "boardId" -> new ObjectId("1e48e10644ae3742baa2d0b9"))
+
+      DataLoader.workflowitems += MongoDBObject(
+        "_id" -> new ObjectId("6f48e10644ae3742baa2d0b0"),
+        "name" -> "name2-2-1",
+        "wipLimit" -> 4,
+        "childId" -> None,
+        "nextItemId" -> new ObjectId("6f48e10644ae3742baa2d0b1"),
+        "boardId" -> new ObjectId("1e48e10644ae3742baa2d0b1"))
+      DataLoader.workflowitems += MongoDBObject(
+        "_id" -> new ObjectId("6f48e10644ae3742baa2d0b1"),
+        "name" -> "name2-2-2",
+        "wipLimit" -> 4,
+        "childId" -> None,
+        "nextItemId" -> None,
+        "boardId" -> new ObjectId("1e48e10644ae3742baa2d0b9"))
+    }
+  }
+
+}
--- kanbanik-web/src/main/java/com/googlecode/kanbanik/client/KanbanikEntryPoint.java
+++ kanbanik-web/src/main/java/com/googlecode/kanbanik/client/KanbanikEntryPoint.java
@@ -6,48 +6,104 @@ import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.googlecode.kanbanik.client.services.ServerCommandInvoker;
 import com.googlecode.kanbanik.client.services.ServerCommandInvokerAsync;
+import com.googlecode.kanbanik.dto.BoardDto;
 import com.googlecode.kanbanik.dto.BoardWithProjectsDto;
 import com.googlecode.kanbanik.dto.ListDto;
+import com.googlecode.kanbanik.dto.ProjectDto;
+import com.googlecode.kanbanik.dto.WorkflowitemDto;
 import com.googlecode.kanbanik.dto.shell.SimpleParams;
 import com.googlecode.kanbanik.dto.shell.VoidParams;
 import com.googlecode.kanbanik.shared.ServerCommand;
 
-public class KanbanikEntryPoint implements EntryPoint{
-	
-	final ServerCommandInvokerAsync serverCommandInvoker = GWT.create(ServerCommandInvoker.class);
-	
+public class KanbanikEntryPoint implements EntryPoint {
+
+	final ServerCommandInvokerAsync serverCommandInvoker = GWT
+			.create(ServerCommandInvoker.class);
+
 	public void onModuleLoad() {
 
-		serverCommandInvoker.<VoidParams, SimpleParams<ListDto<BoardWithProjectsDto>>>invokeCommand(
-				ServerCommand.GET_ALL_BOARDS_WITH_PROJECTS, 
-				new VoidParams(), 
-				new KanbanikAsyncCallback<SimpleParams<ListDto<BoardWithProjectsDto>>>(){
+		serverCommandInvoker
+				.<VoidParams, SimpleParams<ListDto<BoardWithProjectsDto>>> invokeCommand(
+						ServerCommand.GET_ALL_BOARDS_WITH_PROJECTS,
+						new VoidParams(),
+						new KanbanikAsyncCallback<SimpleParams<ListDto<BoardWithProjectsDto>>>() {
 
-			@Override
-			public void success(SimpleParams<ListDto<BoardWithProjectsDto>> result) {
-				buildBoard(RootPanel.get("mainSection"), result);
+							@Override
+							public void success(
+									SimpleParams<ListDto<BoardWithProjectsDto>> result) {
+								buildBoard(RootPanel.get("mainSection"), result);
+							}
+
+						});
+
+	}
+
+	private void buildBoard(RootPanel rootPanel,
+			SimpleParams<ListDto<BoardWithProjectsDto>> result) {
+		VerticalPanel panel = new VerticalPanel();
+
+		for (BoardWithProjectsDto boardWithProjects: result.getPayload().getList()) {
+			BoardDto board = boardWithProjects.getBoard();
+
+			int row = 0;
+			for (ProjectDto project: boardWithProjects.getProjectsOnBoard()) {
+				FlexTable table = new FlexTable();
+				table.setBorderWidth(1);
+				table.setWidget(row, 0, new Label(project.getName()));
+				buildBoard(board.getRootWorkflowitem(), table, row, 0);
+				panel.add(table);
+				row ++;
+			}
+			
+		}
+
+		RootPanel.get("mainSection").add(panel);
+		// table.getCellFormatter().getElement(0, 0).setAttribute("colspan",
+		// "1");
+
+	}
+
+	private void buildBoard(WorkflowitemDto workflowitem, FlexTable table,
+			int row, int column) {
+		if (workflowitem == null) {
+			return;
+		}
+		WorkflowitemDto currentItem = workflowitem;
+
+		while (true) {
+			if (currentItem.getChild() != null) {
+				FlexTable childTable = new FlexTable();
+				childTable.setBorderWidth(1);
+				table.setWidget(row, column, new WorkflowitemPlace(new Label(
+						currentItem.getName()), childTable));
+				buildBoard(currentItem.getChild(), childTable, 0, 0);
+			} else {
+				table.setWidget(row, column, new WorkflowitemPlace(new Label(
+						currentItem.getName()), new FocusPanel()));
 			}
 
-		});
-		
-	}
-	
-	private void buildBoard(RootPanel rootPanel, SimpleParams<ListDto<BoardWithProjectsDto>> result) {
-		FocusPanel panel1 = new FocusPanel();
-		FocusPanel panel2 = new FocusPanel();
-		
-		FlexTable table = new FlexTable();
-		table.setWidget(0, 0, panel1);
-		table.setWidget(0, 1, panel2);
-		
-//		table.getCellFormatter().getElement(0, 0).setAttribute("colspan", "1");
-		
-		panel1.add(new Label("Hello"));
-		panel2.add(new Label("World!"));
-		RootPanel.get("mainSection").add(table);		
+			currentItem = currentItem.getNextItem();
+			if (currentItem == null) {
+				break;
+			}
+
+			column++;
+		}
+
 	}
 
 }
 
+class WorkflowitemPlace extends VerticalPanel {
+
+	public WorkflowitemPlace(Widget header, Widget body) {
+		super();
+		add(header);
+		add(body);
+	}
+
+}
