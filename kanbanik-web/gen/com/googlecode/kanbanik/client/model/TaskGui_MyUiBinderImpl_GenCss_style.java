f70cd8498dcca0bf91b9d657111f4ac8d06556e5
Tomas Jelinek <jelkosz@gmail.com>
2012-06-19 21:02:15 +0200

    Deleted generated code

--- kanbanik-web/gen/com/googlecode/kanbanik/client/model/TaskGui_MyUiBinderImpl_GenCss_style.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.kanbanik.client.model;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface TaskGui_MyUiBinderImpl_GenCss_style extends CssResource {
-  @ClassName("task-toolbar")
-  String taskToolbar();
-}
