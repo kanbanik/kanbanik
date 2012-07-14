ea2fce4b398222a41018632298b6f77648d7ab90
Tomas Jelinek <jelkosz@gmail.com>
2012-07-14 20:37:11 +0200

    Added check to DeleteBoardCommand which ensures, that the board will not be removed when there are projects on it.

--- kanbanik-server/src/main/scala/com/googlecode/kanbanik/commands/DeleteBoardCommand.scala
+++ kanbanik-server/src/main/scala/com/googlecode/kanbanik/commands/DeleteBoardCommand.scala
@@ -5,16 +5,31 @@ import com.googlecode.kanbanik.dto.shell.VoidParams
 import com.googlecode.kanbanik.dto.BoardDto
 import com.googlecode.kanbanik.model.BoardScala
 import org.bson.types.ObjectId
+import com.googlecode.kanbanik.model.KanbanikEntity
+import com.mongodb.casbah.commons.MongoDBObject
 
-class DeleteBoardCommand extends ServerCommand[SimpleParams[BoardDto], FailableResult[VoidParams]] {
+class DeleteBoardCommand extends ServerCommand[SimpleParams[BoardDto], FailableResult[VoidParams]] with KanbanikEntity {
 
   def execute(params: SimpleParams[BoardDto]): FailableResult[VoidParams] = {
-    val board = BoardScala.byId(new ObjectId(params.getPayload().getId()))
-    if (!board.workflowitems.isDefined) {
-      board.delete
-      return new FailableResult(new VoidParams, true, "")
+
+    val boardId = new ObjectId(params.getPayload().getId())
+    if (isOnProject(boardId)) {
+      return new FailableResult(new VoidParams, false, "There are projects on this board. Please remove them from the board first and than delete this board.")
     }
 
-    return new FailableResult(new VoidParams, false, "There are workflowitems on this board. Please delete them first and than delete this board.")
+    val board = BoardScala.byId(boardId)
+    if (board.workflowitems.isDefined) {
+      return new FailableResult(new VoidParams, false, "There are workflowitems on this board. Please delete them first and than delete this board.")
+    }
+
+    board.delete
+    return new FailableResult(new VoidParams, true, "")
+
+  }
+
+  def isOnProject(boardId: ObjectId): Boolean = {
+    using(createConnection) { conn =>
+      return coll(conn, Coll.Projects).findOne(MongoDBObject("boards" -> boardId)).isDefined
+    }
   }
 }
