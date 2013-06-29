63f2ecfe1f8772b0783f551a25525edd29f54d1b
Tomas Jelinek <tjelinek@redhat.com>
2013-06-29 17:46:44 +0200

    Finished migration scripts

--- kanbanik-server/src/main/scala/com/googlecode/kanbanik/migrate/MigrateDb.scala
+++ kanbanik-server/src/main/scala/com/googlecode/kanbanik/migrate/MigrateDb.scala
@@ -7,33 +7,32 @@ import com.googlecode.kanbanik.dto.ManipulateUserDto
 import com.googlecode.kanbanik.commands.CreateUserCommand
 import com.googlecode.kanbanik.dto.shell.SimpleParams
 import com.mongodb.DBObject
-import com.googlecode.kanbanik.model.Board
-import com.googlecode.kanbanik.model.Workflowitem
+import com.googlecode.kanbanik.model._
 import org.bson.types.ObjectId
-import com.googlecode.kanbanik.model.Task
-import com.googlecode.kanbanik.model.Project
 import com.mongodb.BasicDBList
 import com.googlecode.kanbanik.builders.TaskBuilder
 import com.googlecode.kanbanik.dto.TaskDto
 import com.googlecode.kanbanik.commands.MoveTaskCommand
 import com.googlecode.kanbanik.commons._
+import scala.Some
 
 class MigrateDb extends HasMongoConnection {
-  
+
   val versionMigrations = Map(
     1 -> List(new From1To2, new From2To3),
     2 -> List(new From2To3))
 
   def migrateDbIfNeeded {
-    using(createConnection) { conn =>
-      val version = coll(conn, Coll.KanbanikVersion).findOne()
-      if (version.isDefined) {
-        val curVersion = version.get.get("version").asInstanceOf[Int]
-        runAllFrom(curVersion)
-      } else {
-        coll(conn, Coll.KanbanikVersion) += MongoDBObject("version" -> 1)
-        runAllFrom(1)
-      }
+    using(createConnection) {
+      conn =>
+        val version = coll(conn, Coll.KanbanikVersion).findOne()
+        if (version.isDefined) {
+          val curVersion = version.get.get("version").asInstanceOf[Int]
+          runAllFrom(curVersion)
+        } else {
+          coll(conn, Coll.KanbanikVersion) += MongoDBObject("version" -> 1)
+          runAllFrom(1)
+        }
 
     }
   }
@@ -51,16 +50,17 @@ trait MigrationPart extends HasMongoConnection {
   def migrate;
 
   def setVersionTo(version: Int) {
-    using(createConnection) { conn =>
-      coll(conn, Coll.KanbanikVersion).update(MongoDBObject(),
-        $set("version" -> version))
+    using(createConnection) {
+      conn =>
+        coll(conn, Coll.KanbanikVersion).update(MongoDBObject(),
+          $set("version" -> version))
     }
   }
 
 }
 
 class From1To2 extends MigrationPart {
-  
+
   def migrate {
 
     // create a default user
@@ -75,8 +75,6 @@ class From1To2 extends MigrationPart {
     // create the first user
     new CreateUserCommand().execute(new SimpleParams(userDto))
 
-    // add isBalanceWorkflowitems support
-
     setVersionTo(2)
   }
 }
@@ -84,74 +82,113 @@ class From1To2 extends MigrationPart {
 class From2To3 extends MigrationPart {
   private val oldTasksCollection = "tasks"
   private lazy val taskBuilder = new TaskBuilder()
-  
+
   def migrate {
 
-    migrateTasks()
+    val classesOfServices = createDefaultClassesOfService()
+
+    migrateTasks(classesOfServices)
 
     setVersionTo(3)
   }
 
-  def migrateTasks() {
-    using(createConnection) { conn =>
-      val oldTasks = coll(conn, oldTasksCollection).find().map(asOldEntity(_))
-      val newTasks = oldTasks.map(_.asNewTask)
-      var order = 0
-      for (newTask <- newTasks if (newTask.project != null && newTask.workflowitem != null)) {
-        newTask.withOrder(Integer.toString(order)).store
-        order += 100
-      }
-      
-      cleanup
+  def createDefaultClassesOfService() = {
+    Map(2 -> new ClassOfService(
+      None,
+      "Standard",
+      "For typical tasks that flow through the system.",
+      "83ff00",
+      true,
+      1,
+      None
+    ).store) + (0 -> new ClassOfService(
+      None,
+      "Expedite",
+      "For critical tasks. Can break all rules on the system.",
+      "f80070",
+      true,
+      1,
+      None
+    ).store) + (3 -> new ClassOfService(
+      None,
+      "Intangible",
+      "Nice to have but not critical.",
+      "65b1f8",
+      true,
+      1,
+      None
+    ).store) + (1 -> new ClassOfService(
+      None,
+      "Fixed Delivery Date",
+      "Has to be done until specified date.",
+      "ffeb00",
+      true,
+      1,
+      None
+    ).store)
+  }
+
+  def migrateTasks(classesOfService: Map[Int, ClassOfService]) {
+    using(createConnection) {
+      conn =>
+        val oldTasks = coll(conn, oldTasksCollection).find().map(asOldEntity(_))
+        val newTasks = oldTasks.map(_.asNewTask(classesOfService))
+        var order = 0
+        for (newTask <- newTasks if (newTask.project != null && newTask.workflowitem != null)) {
+          newTask.withOrder(Integer.toString(order)).store
+          order += 100
+        }
+
+        cleanup
     }
-    
+
     def cleanup() {
-      using(createConnection) { conn =>
-        coll(conn, oldTasksCollection).remove(MongoDBObject())
+      using(createConnection) {
+        conn =>
+          coll(conn, oldTasksCollection).remove(MongoDBObject())
       }
     }
-    
+
     def asOldEntity(dbObject: DBObject) = {
       new OldTask(
-        Some(dbObject.get(Task.Fields.id.toString()).asInstanceOf[ObjectId]),
-        dbObject.get(Task.Fields.name.toString()).asInstanceOf[String],
-        dbObject.get(Task.Fields.description.toString()).asInstanceOf[String],
-        dbObject.get(Task.Fields.classOfService.toString()).asInstanceOf[Int],
-        dbObject.get(Task.Fields.ticketId.toString()).asInstanceOf[String],
-        {
-          val res = dbObject.get(Task.Fields.version.toString())
-          if (res == null) {
-            1
-          } else {
-            res.asInstanceOf[Int]
-          }
-        },
-        dbObject.get(Task.Fields.workflowitem.toString()).asInstanceOf[ObjectId])
+      Some(dbObject.get(Task.Fields.id.toString()).asInstanceOf[ObjectId]),
+      dbObject.get(Task.Fields.name.toString()).asInstanceOf[String],
+      dbObject.get(Task.Fields.description.toString()).asInstanceOf[String],
+      dbObject.get(Task.Fields.classOfService.toString()).asInstanceOf[Int],
+      dbObject.get(Task.Fields.ticketId.toString()).asInstanceOf[String], {
+        val res = dbObject.get(Task.Fields.version.toString())
+        if (res == null) {
+          1
+        } else {
+          res.asInstanceOf[Int]
+        }
+      },
+      dbObject.get(Task.Fields.workflowitem.toString()).asInstanceOf[ObjectId])
 
     }
   }
 
   class OldTask(val id: Option[ObjectId],
-    val name: String,
-    val description: String,
-    val classOfService: Int,
-    val ticketId: String,
-    val version: Int,
-    val workflowitemId: ObjectId) {
+                val name: String,
+                val description: String,
+                val classOfService: Int,
+                val ticketId: String,
+                val version: Int,
+                val workflowitemId: ObjectId) {
 
-    def asNewTask(): Task = {
-      // just a hack to make it compile
-      null
-//      new Task(
-//        None, // because I want to create a new one
-//        name,
-//        description,
-//        classOfService,
-//        ticketId,
-//        1, // because I basically want to create a new one
-//        "",
-//        findWorkflowitem(),
-//        findProject())
+    def asNewTask(classesOfService: Map[Int, ClassOfService]): Task = {
+      new Task(
+        None, // because I want to create a new one
+        name,
+        description,
+        classesOfService.get(classOfService),
+        ticketId,
+        1, // because I basically want to create a new one
+        "",
+        None,
+        "",
+        findWorkflowitem(),
+        findProject())
 
     }
 
@@ -162,14 +199,15 @@ class From2To3 extends MigrationPart {
     }
 
     def findProject(): Project = {
-      using(createConnection) { conn =>
-        val projects = for (project <- coll(conn, Coll.Projects).find() if (isOnProject(project))) yield project
-        if (projects == null || projects.isEmpty) {
-          null
-        } else {
-          val projectId = projects.next.get(Project.Fields.id.toString())
-          Project().withId(projectId.asInstanceOf[ObjectId])
-        }
+      using(createConnection) {
+        conn =>
+          val projects = for (project <- coll(conn, Coll.Projects).find() if (isOnProject(project))) yield project
+          if (projects == null || projects.isEmpty) {
+            null
+          } else {
+            val projectId = projects.next.get(Project.Fields.id.toString())
+            Project().withId(projectId.asInstanceOf[ObjectId])
+          }
       }
     }
 
@@ -186,4 +224,5 @@ class From2To3 extends MigrationPart {
       }
     }
   }
+
 }
