1787b68759c3859dc1f2167fb637931c3d7b6c21
Tomas Jelinek <tjelinek@redhat.com>
2014-01-08 21:31:19 +0100

    added the rest of DTOs to scala part - not yet used

--- kanbanik-server/src/main/scala/com/googlecode/kanbanik/dtos/package.scala
+++ kanbanik-server/src/main/scala/com/googlecode/kanbanik/dtos/package.scala
@@ -12,6 +12,26 @@ package object dtos {
 
   case class ClassOfServiceDto(id: Option[String], name: String, description: String, colour: String, version: Int, sessionId: Option[String])
 
+  case class ProjectDto(name: String, id: String, boards: List[BoardDto], version: Int)
+
+  object WorkfloVerticalSizing extends Enumeration {
+    val BALANCED = Value(0)
+    val MIN_POSSIBLE = Value(1)
+  }
+
+  object WorkflowitemType extends Enumeration {
+    val HORIZONTAL = Value("H")
+    val VERTICAL = Value("V")
+  }
+
+  case class BoardDto(id: String, version: Int, name: String, workflowVerticalSizing: Int, workflow: Option[WorkflowDto], showUserPictureEnabled: Boolean, tasks: Option[List[TaskDto]])
+
+  case class WorkflowDto(id: String, workflowitems: Option[List[WorkflowitemDto]], board: BoardDto)
+
+  case class WorkflowitemDto(name: String, id: String, wipLimit: Option[Int], itemType: String, version: Int, nestedWorkflow: Option[WorkflowDto], parentWorkflow: Option[WorkflowDto], verticalSize: Option[Int])
+
+  case class TaskDto(id: String, name: String, description: String, classOfService: ClassOfServiceDto, ticketId: String, workflowitem: WorkflowitemDto, version: Int, project: ProjectDto, assignee: Option[UserDto], dueDate: Option[String])
+
   case class ErrorDto(errorMessage: String)
 
   case class StatusDto(success: Boolean, reason: Option[String])
