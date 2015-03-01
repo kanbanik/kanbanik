c6caa9240292e3d3ec4eb39ddba68c15414500b9
Tomas Jelinek <jelkosz@gmail.com>
2015-03-01 20:32:17 +0100

    Removed the not needed SuggestBox and replaced by simple TextBox hiding the widgets directly

--- kanbanik-web/src/main/java/com/googlecode/kanbanik/client/components/filter/PanelWithCheckboxes.java
+++ kanbanik-web/src/main/java/com/googlecode/kanbanik/client/components/filter/PanelWithCheckboxes.java
@@ -3,14 +3,15 @@ package com.googlecode.kanbanik.client.components.filter;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
 
 public class PanelWithCheckboxes extends Composite {
 
@@ -20,57 +21,42 @@ public class PanelWithCheckboxes extends Composite {
     @UiField
     FlowPanel contentPanel;
 
+    // todo optimize this
     @UiField
     Button allButton;
 
+    // todo optimize this
     @UiField
     Button noneButton;
 
-    @UiField(provided = true)
-    SuggestBox filterBox;
+    @UiField
+    TextBox filterBox;
 
     public PanelWithCheckboxes() {
 
-        filterBox = new SuggestBox(createOracle());
-        filterBox.addValueChangeHandler(new ValueChangeHandler<String>() {
+        initWidget(uiBinder.createAndBindUi(this));
+
+        filterBox.addKeyUpHandler(new KeyUpHandler() {
             @Override
-            public void onValueChange(ValueChangeEvent<String> valueChangeEvent) {
-                valueChangeEvent.getValue();
+            public void onKeyUp(KeyUpEvent event) {
+                String filterText = "";
+                if (filterBox.getText() != null) {
+                    filterText = filterBox.getText().toLowerCase().trim();
+                }
+
                 for (int i = 0; i < contentPanel.getWidgetCount(); i++) {
                     Widget w = contentPanel.getWidget(i);
                     if (w instanceof FilterCheckBox) {
-                        String candidate = ((FilterCheckBox) w).provideText();
-                        String filter = valueChangeEvent.getValue();
-                        if (filter == null || "".equals(filter)) {
-                            w.setVisible(true);
-                        }
-
-                        if (candidate == null || filter == null) {
-                            return;
-                        }
-                        if (candidate.toLowerCase().equals(filter.toLowerCase())) {
+                        final String text = ((FilterCheckBox) w).provideText();
+                        if (text != null && text.toLowerCase().contains(filterText)) {
                             w.setVisible(true);
                         } else {
                             w.setVisible(false);
                         }
-
                     }
                 }
             }
         });
-
-        filterBox.addFocusListener(new FocusListener() {
-            @Override
-            public void onFocus(Widget widget) {
-                filterBox.showSuggestionList();
-            }
-
-            @Override
-            public void onLostFocus(Widget widget) {
-            }
-        });
-
-        initWidget(uiBinder.createAndBindUi(this));
 
         allButton.addClickHandler(new ClickHandler() {
             @Override
@@ -88,54 +74,6 @@ public class PanelWithCheckboxes extends Composite {
 
         allButton.setText("all");
         noneButton.setText("none");
-    }
-
-    private SuggestOracle createOracle() {
-        return new SuggestOracle() {
-            @Override
-            public void requestSuggestions(Request request, Callback callback) {
-                doFindSuggestions(request, callback, true);
-            }
-
-            @Override
-            public void requestDefaultSuggestions(Request request, Callback callback) {
-                doFindSuggestions(request, callback, false);
-            }
-
-            private void doFindSuggestions(Request request, Callback callback, boolean check) {
-                List<Suggestion> res = new ArrayList<Suggestion>();
-                fillSggestions(request, res, check);
-
-                Response response = new Response(res);
-                callback.onSuggestionsReady(request, response);
-            }
-
-
-
-            private void fillSggestions(Request request, List<Suggestion> res, boolean check) {
-                for (int i = 0; i < contentPanel.getWidgetCount(); i++) {
-                    Widget w = contentPanel.getWidget(i);
-                    if (w instanceof FilterCheckBox) {
-                        final String text = ((FilterCheckBox) w).provideText();
-                        if (!check || (text != null && text.toLowerCase().contains(request.getQuery().toLowerCase()))) {
-                            res.add(new Suggestion() {
-                                @Override
-                                public String getDisplayString() {
-                                    return text;
-                                }
-
-                                @Override
-                                public String getReplacementString() {
-                                    return text;
-                                }
-                            });
-
-                        }
-                    }
-                }
-            }
-
-        };
     }
 
     private void setAllSelected(boolean selected) {
