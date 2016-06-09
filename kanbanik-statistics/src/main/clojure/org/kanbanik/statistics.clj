81510bb754716c23ad24a770bb90fd8e1a93f922
Tomas Jelinek <jelkosz@gmail.com>
2016-06-09 16:50:10 +0200

    Fixed tests of the group-by-timeframe function

--- kanbanik-statistics/src/main/clojure/org/kanbanik/statistics.clj
+++ kanbanik-statistics/src/main/clojure/org/kanbanik/statistics.clj
@@ -3,8 +3,8 @@
 (defn group-by-timeframe [stream timeframe]
   "Gets a vector of task related events sorted by time and groups them according to given time frame.
   Timeframe: in seconds
-  Input: [event1, event2, event3, event4....]
-  Output [[event1, event2...], [event3, event4...]...]"
+  Input: [event1 event2 event3 event4....]
+  Output {timeframe1 [event1 event2...] timeframe2 [event3 event4...]...]}"
   (if (= (count stream) 0)
     []
 ; the base time starts one millisecond before the first item from the stream
--- kanbanik-statistics/src/test/clojure/org/kanbanik/statistics_tests.clj
+++ kanbanik-statistics/src/test/clojure/org/kanbanik/statistics_tests.clj
@@ -4,27 +4,25 @@
 
 (testing "Grouping stream of tasks by timeframes"
   (testing "Empty input returns empty output"
-    (is (= [[]] (group-streams-by-time [], 10)))
+    (is (= [] (group-streams-by-time [], 10)))
     )
 
   (testing "One element input returns one element output"
-    (is (= [[{:timestamp 1464509670 :name "name 1"}]]
-           (group-streams-by-time [{:timestamp 1464509670 :name "name 1"}] 10)))
+    (is (= {1.0 [{:timestamp 1464509670 :name "name 1"}] }
+             (group-by-timeframe [{:timestamp 1464509670 :name "name 1"}] 10)))  
     )
 
   (testing "Two element input in the same chunk"
-    (is (= [[{:timestamp 1464509670 :name "name 1"}
-             {:timestamp 1464509671 :name "name 2"}]]
-           (group-streams-by-time [{:timestamp 1464509670 :name "name 1"}
+    (is (= {1.0 [{:timestamp 1464509670 :name "name 1"}
+               {:timestamp 1464509671 :name "name 2"}]}
+           (group-by-timeframe [{:timestamp 1464509670 :name "name 1"}
                                    {:timestamp 1464509671 :name "name 2"}] 10)))
     )
   
-(comment
     (testing "Two element input in different chunks"
-      (is (= [[{:timestamp 1464509670 :name "name 1"}]
-              [{:timestamp 1464509681 :name "name 2"}]]
-             (group-streams-by-time [{:timestamp 1464509670 :name "name 1"}
+      (is (= {1.0 [{:timestamp 1464509670 :name "name 1"}]
+              2.0 [{:timestamp 1464509681 :name "name 2"}]} 
+             (group-by-timeframe [{:timestamp 1464509670 :name "name 1"}
                                      {:timestamp 1464509681 :name "name 2"}] 10)))
-      )
-   )
+      ) 
   )
